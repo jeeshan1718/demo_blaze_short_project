9f1e478e60127396ba65c3f1f53fb1c29394ca95
Jeeshan Ahmad <116867098+jeeshan1718@users.noreply.github.com>
2026-02-07 22:54:12 +0530

    Update conftest.py

--- DEMO_BLAZE/conftest.py
+++ DEMO_BLAZE/conftest.py
@@ -1,10 +1,15 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture()
 def setup():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    options = Options()
+    options.add_argument("--headless")          # 👈 main cheez
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
     driver.get("https://www.demoblaze.com")
     yield driver
     driver.quit()
