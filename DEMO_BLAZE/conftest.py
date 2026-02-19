c18fd60b8a610e34af7ffc1ca2153dce3608a723
Jeeshan Ahmad <116867098+jeeshan1718@users.noreply.github.com>
2026-02-19 12:51:26 +0530

    Update conftest.py

--- DEMO_BLAZE/conftest.py
+++ DEMO_BLAZE/conftest.py
@@ -1,26 +1,24 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture()
 def setup():
     options = Options()
-    options.add_argument("--headless")  
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+
     driver.get("https://www.demoblaze.com")
     yield driver
     driver.quit()
-
-
-def get_driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1920,1080")
-
-    driver = webdriver.Chrome(options=chrome_options)
-    return driver
