a808daa574b410c330109eaa80f83e76f9b8d2da
Jeeshan Ahmad <116867098+jeeshan1718@users.noreply.github.com>
2026-02-19 12:10:47 +0530

    Update conftest.py

--- DEMO_BLAZE/conftest.py
+++ DEMO_BLAZE/conftest.py
@@ -13,3 +13,14 @@ def setup():
     driver.get("https://www.demoblaze.com")
     yield driver
     driver.quit()
+
+
+def get_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
